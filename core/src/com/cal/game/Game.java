417833a647d88f5ee5b93eeffed51cbfa4f62375
Sean J <geniusninja88@gmail.com>
2015-05-24 02:36:33 -0700

    Added Levels

--- core/src/com/cal/game/Game.java
+++ core/src/com/cal/game/Game.java
@@ -24,7 +24,7 @@ public class Game extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		level = new Level("Levels/testlevel.xml");
+		level = new Level("Levels/Level10.xml");
 		background = new Texture(Gdx.files.internal("Game Pieces/Background.png"));
 		border = new Texture(Gdx.files.internal("Animation/Border.png"));
 	}
